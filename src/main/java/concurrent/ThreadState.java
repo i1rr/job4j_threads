825683d67fdc5366036fc2ebebc10295044004dc
Ivan Rein <61737800+i1rr@users.noreply.github.com>
2021-10-30 19:20:02 +1100

    Thread.getState

---
+++ src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,28 @@
+package concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+
+        Thread second = new Thread(
+                () -> {}
+        );
+
+        System.out.println("First thread: " + first.getState());
+        System.out.println("Second thread: " + second.getState());
+
+        first.start();
+        second.start();
+
+        while (first.getState() != Thread.State.TERMINATED
+                && second.getState() != Thread.State.TERMINATED) {
+            System.out.println("First thread: " + first.getState());
+            System.out.println("Second thread: " + second.getState());
+        }
+        System.out.println("First thread: " + first.getState());
+        System.out.println("Second thread: " + second.getState());
+        System.out.println("\nBoth threads are terminated.");
+    }
+}
