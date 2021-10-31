85f9c35895ef96764537a8b40bc91939e0002472
Ivan Rein <61737800+i1rr@users.noreply.github.com>
2021-10-31 15:28:32 +1100

    Thread#start()

--- src/main/java/concurrent/ConcurrentOutput.java
+++ src/main/java/concurrent/ConcurrentOutput.java
@@ -1,11 +1,14 @@
 package concurrent;
 
 public class ConcurrentOutput {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName()));
             Thread second = new Thread(
                     () -> System.out.println(Thread.currentThread().getName()));
 
+            another.start();
             second.start();
 
             System.out.println(Thread.currentThread().getName());
