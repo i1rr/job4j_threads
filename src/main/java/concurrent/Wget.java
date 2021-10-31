ebceb77d5af46151d08230878f1148b3c179eb2e
Ivan Rein <61737800+i1rr@users.noreply.github.com>
2021-10-31 17:40:21 +1100

    Download speed limit

--- src/main/java/concurrent/Wget.java
+++ src/main/java/concurrent/Wget.java
@@ -1,15 +1,51 @@
 package concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        try {
-            int index = 0;
-            while (index < 101) {
-                System.out.print("\rLoading : " + index++ + "%");
-                Thread.sleep(1000);
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+    private final String fileName;
+
+    public Wget(String url, int speed, String filename) {
+        this.url = url;
+        this.speed = speed;
+        this.fileName = filename;
+    }
+
+    @Override
+    public void run() {
+
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long startTime = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long timeSpent = System.currentTimeMillis() - startTime;
+                if (timeSpent < speed) {
+                    try {
+                        Thread.sleep(speed - timeSpent / 1000);
+                    } catch (InterruptedException ie) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } catch (IOException io) {
+            io.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        String fileName = args[2];
+        Thread wget = new Thread(new Wget(url, speed, fileName));
+        wget.start();
+        wget.join();
     }
 }
