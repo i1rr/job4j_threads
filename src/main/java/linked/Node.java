a138bf1208673d8142b60d0c05dbc2c48eee5012
Ivan Rein <61737800+i1rr@users.noreply.github.com>
2021-11-06 20:24:19 +1100

    Immutable object

---
+++ src/main/java/linked/Node.java
@@ -0,0 +1,19 @@
+package linked;
+
+public class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
