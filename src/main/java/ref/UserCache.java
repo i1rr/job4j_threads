90de481da4ae21ff8cb912fd2cd5e607ff09d67a
Ivan Rein <61737800+i1rr@users.noreply.github.com>
2021-11-07 17:44:19 +1100

    Thread with no global resources.

---
+++ src/main/java/ref/UserCache.java
@@ -0,0 +1,28 @@
+package ref;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@NotThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        ArrayList<User> userList = new ArrayList<>();
+        users.values().forEach(user -> userList.add(User.of(user.getName())));
+        return userList;
+    }
+}
