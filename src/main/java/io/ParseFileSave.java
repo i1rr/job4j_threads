71d3ab1e1b81027d54b794f47cfc6819d43b1ab9
Ivan Rein <61737800+i1rr@users.noreply.github.com>
2021-11-08 19:57:08 +1100

    Visibility. Global res outside of the crit section.. Comment fix.. Second try

--- src/main/java/io/ParseFileSave.java
+++ src/main/java/io/ParseFileSave.java
@@ -3,13 +3,6 @@ package io;
 import java.io.*;
 
 public final class ParseFileSave {
-//- Методы getContent написаны в стиле копипаста.
-// Нужно применить шаблон стратегия. content(Predicate<Character> filter)
-//
-//3. Загрузите код в репозиторий. Оставьте ссылку на коммит.
-//
-//4. Переведите ответственного на Петра Арсентьева.
-
     private final File file;
 
     public ParseFileSave(File file) {
