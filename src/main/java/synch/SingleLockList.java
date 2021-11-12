ad6ff3223b7542eba5d8b5ce70805f1a7a61ddf5
Ivan Rein <61737800+i1rr@users.noreply.github.com>
2021-11-12 19:52:35 +1100

    SingleLockList

--- src/main/java/synch/SingleLockList.java
+++ src/main/java/synch/SingleLockList.java
@@ -1,27 +1,35 @@
 package synch;
 
+import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 @ThreadSafe
 public class SingleLockList<T> implements Iterable<T> {
+    @GuardedBy("this")
     private final List<T> list;
 
     public SingleLockList(List<T> list) {
-        this.list = (List) list.clone();
+        this.list = copy(list);
     }
 
-    public void add(T value) {
+    public synchronized void add(T value) {
+        list.add(value);
     }
 
-    public T get(int index) {
-        return null;
+    public synchronized T get(int index) {
+        return list.get(index);
+    }
+
+    private synchronized List<T> copy(List<T> list) {
+        return new ArrayList<>(list);
     }
 
     @Override
     public synchronized Iterator<T> iterator() {
-        return copy(this.array).iterator();
+        return copy(this.list).iterator();
     }
 }
--- src/test/java/synch/SingleLockListTest.java
+++ src/test/java/synch/SingleLockListTest.java
@@ -2,16 +2,17 @@ package synch;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.TreeSet;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 public class SingleLockListTest {
 
     @Test
     public void add() throws InterruptedException {
-        SingleLockList<Integer> list = new SingleLockList<>();
+        SingleLockList<Integer> list = new SingleLockList<>(new ArrayList<>());
         Thread first = new Thread(() -> list.add(1));
         Thread second = new Thread(() -> list.add(2));
         first.start();
