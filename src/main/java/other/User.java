edeb302c5f8626ae5eae8e60bca338ee9eccd983
Ivan Rein <61737800+i1rr@users.noreply.github.com>
2021-11-12 19:05:18 +1100

    UserStorage to map

--- src/main/java/other/User.java
+++ src/main/java/other/User.java
@@ -3,6 +3,8 @@ package other;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
+import java.util.Objects;
+
 @ThreadSafe
 public final class User {
     private final int id;
@@ -25,5 +27,22 @@ public final class User {
 
     public synchronized void setBalance(int balance) {
         this.balance = balance;
+    }
+
+    @Override
+    public synchronized boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id && balance == user.balance;
+    }
+
+    @Override
+    public synchronized int hashCode() {
+        return Objects.hash(id, balance);
     }
 }
