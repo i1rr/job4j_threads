0dc142f65e43ed496b8d7e08c84d7ac7b1ac0acf
Ivan Rein <61737800+i1rr@users.noreply.github.com>
2021-11-20 17:13:57 +1100

    Junit test for blocking queue v2 checkStyle fix

--- src/test/java/blockingqueue/SimpleBlockingQueueTest.java
+++ src/test/java/blockingqueue/SimpleBlockingQueueTest.java
@@ -39,7 +39,7 @@ public class SimpleBlockingQueueTest {
         producer.join();
         consumer.interrupt();
         consumer.join();
-        assertThat(buffer, is(Arrays.asList(0, 1, 2 ,3 ,4 ,5, 6, 7, 8, 9)));
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));
     }
 
     @Test
